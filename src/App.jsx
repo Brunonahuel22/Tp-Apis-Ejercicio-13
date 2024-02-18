570def20abf30a6d04bd4cae3eac1f4aee2d0ba1
Bruno Cordoba <brunonahuel87@gmail.com>
2024-02-18 13:01:25 -0300

    agregue la logica para buscar y tambien  le di los estilos a la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Clima from "./components/Clima.jsx";
+import './css/app.css'
 
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [precarga, SetPreCarga] = useState("");
+  const [lugar, setLugar] = useState("florida");
+  const [datoClima, setDatoClima] = useState("");
+
+  useEffect(() => {
+    consultarApi();
+  }, [lugar]);
+
+  const consultarApi = async () => {
+    let link = `https://api.openweathermap.org/data/2.5/weather?q=${lugar}&lang=es&appid=223cd5ecc6ca779932f9cb1a1fb1ed43`;
+    const resultado = await fetch(link);
+
+    const dato = await resultado.json();
+    setDatoClima(dato);
+  };
+
+  const handleChange = (e) => {
+    SetPreCarga(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLugar(precarga);
+    SetPreCarga('')
+  };
 
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <h1 className="text-center text-white p-3">App Temperatura</h1>
+
+      <Container className=" my-3">
+        <Form
+          className="p-4 border-white text-center form"
+          onSubmit={handleSubmit}
+        >
+          <Form.Group className="mb-3 " controlId="formBasicEmail">
+            <Form.Control
+              type="text"
+              placeholder="Ingrese una Ciudad"
+              className="p-3"
+              onChange={handleChange}
+              value={precarga}
+            />
+          </Form.Group>
+        </Form>
+
+        <Clima datosClima={datoClima} />
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import '../css/clima.css'
 
-const Clima = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Clima = ({datosClima}) => {
+ 
+
+  return (
+    <section className="p-3">
+      <article className=" text-center p-2">
+        <div><img className="w-25" src="https://w7.pngwing.com/pngs/453/300/png-transparent-animated-sun-caricature-drawing-sun-photography-orange-computer-wallpaper-thumbnail.png" alt="" /></div>
+        <div className="temperatura"><p ></p></div>
+        <div className="temperatura"><p >descripcion</p></div>
+        <div id="ciudad">{datosClima.name}</div>
+      </article>
+    </section>
+  );
 };
 
 export default Clima;
